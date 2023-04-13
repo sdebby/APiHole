d6200d3a7cafaae7203d7118ce28b6ce295b7813
ShmulikD <shmulik.debby@gmail.com>
2023-04-13 19:23:14 +0300

    update code

--- APiHole/example.py
+++ APiHole/example.py
@@ -13,7 +13,7 @@ print(PiHole.GetStatus(PiIP,PiHoleAPI))
 print(PiHole.GetGravity(PiIP,PiHoleAPI))
 
 # get PiHole top items, split and convert to list
-Items=PiHole.GetTopItems(PiIP,PiHoleAPI)
+Items=PiHole.GetTopItems(PiIP,PiHoleAPI,7)
 if not len(Items) == 0:
     ItemsTop=Items['top_queries']
     resultListTOP = list(ItemsTop.items())
@@ -41,5 +41,32 @@ print(PiHole.GetTopClients(PiIP,PiHoleAPI))
 # Add google.com to white list
 print(PiHole.AddWhite(PiIP,PiHoleAPI,'google.com'))
 
+# Add google.com to block list
+print(PiHole.AddBlock(PiIP,PiHoleAPI,'google.com'))
+
 # Print API version (good for testing communication)
 print(PiHole.GetVer(PiIP))
+
+# Print recent blocked domain
+print(PiHole.GetRecentBlocked(PiIP,PiHoleAPI))
+
+# Print destinations in %
+print(PiHole.GetDestination(PiIP,PiHoleAPI))
+
+# Print query types in %
+print(PiHole.GetQueryTypes(PiIP,PiHoleAPI))
+
+# Print clients names
+print(PiHole.GetClientNames(PiIP,PiHoleAPI))
+
+# Print data of clients over time
+print(PiHole.GetOverTimeDataClients(PiIP,PiHoleAPI))
+
+# Print pi hole DNS port
+print(PiHole.GetDnsPort(PiIP,PiHoleAPI))
+
+# Print pi hole chche info
+print(PiHole.GetCacheInfo(PiIP,PiHoleAPI))
+
+# Print pi hole data of 10 min 
+print(PiHole.GetOverTimeData10mins(PiIP,PiHoleAPI))
