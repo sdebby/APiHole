57360bd1263d2bc2ed947b3969411e59beadb8dc
Shmulik <sdebby@users.noreply.github.com>
2024-06-24 21:34:56 +0300

    Update setup.py
    update revision 0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description1 = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.3'
 DESCRIPTION = 'API module for Pi Hole'
 LONG_DESCRIPTION = 'Python API module for comunicating with Pi Hole server, including all functions.'
 AUTHOR='Shmulik Debby'
